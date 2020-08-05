8ede418e3feca33dc44048bc473309f2ea4384ed
NicollasDileo06 <nicgato2013@gmail.com>
2020-08-05 12:02:07 -0300

    dll e dml boletim

--- DML Boletim.sql
+++ DML Boletim.sql
@@ -6,8 +6,8 @@ USE boletim
 
 INSERT INTO Aluno (Nome, RA, Idade) VALUES ('Edu','RA1313433',16); 
 --INSERT INTO Aluno (Nome, RA, Idade) VALUES ('RA1313413','Caua',16); Pode-se inverter a ordem
-INSERT INTO Materia (Titulo) VALUES ('Matemática');
-INSERT INTO Materia (Titulo) VALUES ('Português');
+INSERT INTO Materia (Titulo) VALUES ('Matemï¿½tica');
+INSERT INTO Materia (Titulo) VALUES ('Portuguï¿½s');
 INSERT INTO Trabalho (Nota, DataEntrega, IdAluno, IdMateria) VALUES (8.6, '2020-08-05T23:59:59', 1,2)
 INSERT INTO Trabalho (Nota, DataEntrega, IdAluno, IdMateria) VALUES (8.6, '2020-08-05T23:59:59', 2,1)
 
@@ -24,13 +24,13 @@ RA ='Update'
 WHERE idAluno = 8 */
 
 
---Com update sem Where todos os elementos são mudados
+--Com update sem Where todos os elementos sï¿½o mudados
 
 -- DELETE - Excluir 
 
 DELETE FROM Aluno WHERE idAluno = 5;
 
---WHERE É IMPORTANTE POIS ELE É NECESSARIO PARA PODER SELECIONAR APENAS O QUE QUER EXCLUIR OU MUDAR
+--WHERE ï¿½ IMPORTANTE POIS ELE ï¿½ NECESSARIO PARA PODER SELECIONAR APENAS O QUE QUER EXCLUIR OU MUDAR
 -- DELETE FROM Trabalho;
 -- DELETE FROM Aluno;
 -- DELETE FROM Materia;
@@ -45,9 +45,11 @@ SELECT * FROM Trabalho;
 
 
 /*CREATE TABLE teste(
-CPF VARCHAR(16) UNIQUE NOT NULL,     COM UNIQUE SÓ PODE SE USAR UMA VEZ O VALOR, 
+CPF VARCHAR(16) UNIQUE NOT NULL,     COM UNIQUE Sï¿½ PODE SE USAR UMA VEZ O VALOR, 
 Lorem ipsum                          ASSIM N DA PRA TER VALORES COMO NOME REPETIDOS NA TABELA. 
 );
 */
 
 ALTER TABLE Trabalho ALTER COLUMN Nota DECIMAL (10, 2)
+
+/**/
